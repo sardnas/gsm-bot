7447d19700170f3e98a9ed54af7351f1a74f71a6
Sandra Reinecke <sandra.reinecke@consid.se>
2023-06-12 17:00:09 +0200

    crazy bad solution

--- gsm-bot-python/board.py
+++ gsm-bot-python/board.py
@@ -11,26 +11,48 @@ class board(pygame.sprite.Sprite):
         #self.image = pygame.Surface((50, 50))
         #self.image.fill((246, 224, 210))  # the color
         self.board = self.createBoard()
+        self.pink = -1
+        self.purple = -1
+        self.blue = -1
+        self.green = -1
+        self.validInput = True
+        self.initialBoard
 
     def getBoard(self):
         return self.board
+
+    def getValidInput(self):
+        return self.validInput
+
+    def undoBoard(self):
+        self.board = self.initialBoard
+
+    def checkForWin(self):
+        if self.pink == 0 and self.purple == 0 and self.blue == 0 and self.green == 0:
+            return True
+        else:
+            return False
+    def checkForLose(self):
+        if self.pink == 1 or self.purple == 1 or self.blue == 1 or self.green == 1:
+            return True
+        else:
+            return False
     def updateBoard(self, coordinate):
         if self.board[coordinate[0]][coordinate[1]] == 1:
             self.clearNeighbours(1, coordinate, True)
-
-        elif board[coordinate[0]][coordinate[1]] == 2:
+        elif self.board[coordinate[0]][coordinate[1]] == 2:
             self.clearNeighbours(2, coordinate, True)
-
-        elif board[coordinate[0]][coordinate[1]] == 3:
+        elif self.board[coordinate[0]][coordinate[1]] == 3:
             self.clearNeighbours(3, coordinate, True)
-
-        elif board[coordinate[0]][coordinate[1]] == 4:
+        elif self.board[coordinate[0]][coordinate[1]] == 4:
             self.clearNeighbours(4, coordinate, True)
-
         else:
             print("Please choose correct value")
+            self.validInput = False
+            return
 
         self.board = self.moveDown()
+        self.board = self.moveRight()
 
     def moveDown(self):
         board_copy = [[None] * len(self.board[0]) for _ in range(len(self.board))]  # Create a copy of the board
@@ -55,9 +77,55 @@ class board(pygame.sprite.Sprite):
 
         return board_copy
 
+    def moveRight(self):
+        counter = []
+        for i in range(len(self.board)):
+            if self.board[10][i] == 0 and i > 0:
+                counter.append(i)
+
+        for i in range(len(counter)):
+            self.board = self.moveRightHelper(counter[i])
+
+        return self.board
+    def moveRightHelper(self, column):
+        board_copy = [[None] * len(self.board[0]) for _ in range(len(self.board))]  # Create a copy of the board
+
+        for i in range(len(self.board)):
+            board_copy[i] = self.board[i].copy()
+
+        # Make the movable part blanc
+        for i in range(len(board_copy)):
+            for j in range(column):
+                board_copy[i][j] = 0
+
+        for i in range(len(board_copy)):
+            for j in range(column):
+                board_copy[i][j+1] = self.board[i][j]
+
+        return board_copy
+
     def createBoard(self):
-        print(random.choice([1, 2, 3, 4], size=(11, 11)))
-        return random.choice([1, 2, 3, 4], size=(11, 11))
+        newBoard = random.choice([1, 2, 3, 4], size=(11, 11))
+        pink = 0
+        purple = 0
+        blue = 0
+        green = 0
+        for i in range(len(newBoard)):
+            for j in range(len(newBoard)):
+                if newBoard[i][j] == 1:
+                    pink = pink + 1
+                elif newBoard[i][j] == 2:
+                    purple = purple + 1
+                elif newBoard[i][j] == 3:
+                    blue = blue + 1
+                elif newBoard[i][j] == 4:
+                    green = green + 1
+        self.initialBoard = newBoard
+        self.pink = pink
+        self.purple = purple
+        self.blue = blue
+        self.green = green
+        return newBoard
 
     def validCoord(self, coordinate):
         if coordinate[0] < 0 or coordinate[0] > 10 or coordinate[1] < 0 or coordinate[1] > 10:
@@ -67,8 +135,17 @@ class board(pygame.sprite.Sprite):
 
     def clearNeighbours(self, value, coordinate, firstCheck):
         if self.board[coordinate[0]][coordinate[1]] == value:
+            self.validInput = not firstCheck
             if not firstCheck:
                 self.board[coordinate[0]][coordinate[1]] = 0
+                if value == 1:
+                    self.pink = self.pink-1
+                elif value == 2:
+                    self.purple = self.purple-1
+                elif value == 3:
+                    self.blue = self.blue-1
+                elif value == 4:
+                    self.green = self.green-1
 
             #over
             newCoord = [coordinate[0] - 1, coordinate[1]]
--- gsm-bot-python/main.py
+++ gsm-bot-python/main.py
@@ -1,7 +1,7 @@
 import pygame
 from board import board
 from settings import size, FPS, PINK, PURPLE, BLUE, GREEN, CLEAR
-
+import random
 # initialize pygame and create window
 pygame.init()
 screen = pygame.display.set_mode(size)
@@ -25,28 +25,69 @@ def game_loop():
                     running = False
 
         # Draw / render
+        font = pygame.font.SysFont('Arial', 20)
         screen.fill((249, 249, 249))
         x = 0
         y = 0
+
         for i in range(len(gameBoard.getBoard())):
             for j in range(len(gameBoard.getBoard()[0])):
+                coordinate_string = "[{},{}]".format(i, j)
                 if gameBoard.getBoard()[i][j] == 0:
                     pygame.draw.rect(screen, CLEAR, pygame.Rect(x, y, 50, 50))
+                    screen.blit(font.render(coordinate_string, True, (249, 249, 249)), (x, y))
                 elif gameBoard.getBoard()[i][j] == 1:
                     pygame.draw.rect(screen, PINK, pygame.Rect(x, y, 50, 50))
+                    screen.blit(font.render(coordinate_string, True, (249, 249, 249)), (x, y))
                 elif gameBoard.getBoard()[i][j] == 2:
                     pygame.draw.rect(screen, PURPLE, pygame.Rect(x, y, 50, 50))
+                    screen.blit(font.render(coordinate_string, True, (249, 249, 249)), (x, y))
                 elif gameBoard.getBoard()[i][j] == 3:
                     pygame.draw.rect(screen, BLUE, pygame.Rect(x, y, 50, 50))
+                    screen.blit(font.render(coordinate_string, True, (249, 249, 249)), (x, y))
                 elif gameBoard.getBoard()[i][j] == 4:
                     pygame.draw.rect(screen, GREEN, pygame.Rect(x, y, 50, 50))
+                    screen.blit(font.render(coordinate_string, True, (249, 249, 249)), (x, y))
                 x = x + 50
             x = 0
             y = y + 50
 
-
-        pygame.draw.rect(screen, PINK, pygame.Rect(x, y, 50, 50))
-
         pygame.display.flip()
 
-game_loop()
+        remove_x = int(input('Enter x-coordinate\n'))
+        remove_y = int(input('Enter y-coordinate\n'))
+        coordinates = [remove_x, remove_y]
+
+        gameBoard.updateBoard(coordinates)
+
+def solutionAlghorithm():
+    searching = True
+    arrayOfCoordinates = []
+    invalidCount = 0
+
+    while searching:
+        randomX = random.choice([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
+        randomY = random.choice([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
+        randomCoordinates = [randomX, randomY]
+
+        arrayOfCoordinates.append(randomCoordinates)
+
+        gameBoard.updateBoard(randomCoordinates)
+
+        if not gameBoard.validInput:
+            invalidCount = invalidCount + 1
+
+        if gameBoard.checkForLose() or invalidCount > 100:
+            gameBoard.undoBoard()
+            print('GAME OVER')
+            print(arrayOfCoordinates)
+            arrayOfCoordinates = []
+            invalidCount = 0
+
+        if gameBoard.checkForWin():
+            print('SUCCESS')
+            print(arrayOfCoordinates)
+            searching = False
+
+
+solutionAlghorithm()
