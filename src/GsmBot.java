b660e4155f23f3cc352a1b75fb974bef41260cbe
Sandra Reinecke <sandra.reinecke@consid.se>
2023-06-14 12:21:20 +0200

    findStartCoordinate

--- src/GsmBot.java
+++ src/GsmBot.java
@@ -2,6 +2,7 @@ import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.io.IOException;
 import java.io.File;
+import java.util.ArrayList;
 import javax.imageio.ImageIO;
 
 
@@ -11,13 +12,28 @@ public class GsmBot {
 
     }
 
-    public void takeScreenShot() throws InterruptedException, AWTException {
+    public BufferedImage takeScreenShot() throws AWTException {
         Robot robot = new Robot();
         Rectangle capture = new Rectangle(Toolkit.getDefaultToolkit().getScreenSize());
         BufferedImage image = robot.createScreenCapture(capture);
+        System.out.println("Took shot. Height: ");
+        System.out.println(image.getHeight());
+        return image;
     }
 
-    private void findGameBoard(BufferedImage screenShotImage) throws IOException {
+    private void findGameBoard(BufferedImage screenShotImage) {
+
+
+    }
+
+    public void findStartCoordinate(BufferedImage screenShotImage) throws IOException {
+        ArrayList<Integer> leftTopCorner = findMatch(screenShotImage);
+        System.out.print(leftTopCorner.get(0));
+        System.out.print(",");
+        System.out.print(leftTopCorner.get(1));
+    }
+
+    private ArrayList<Integer> findMatch(BufferedImage screenShotImage) throws IOException {
         BufferedImage pinkBubble = null;
         pinkBubble = ImageIO.read(new File("images/pinkBubble.jpg"));
 
@@ -30,15 +46,31 @@ public class GsmBot {
         BufferedImage blueBubble = null;
         blueBubble = ImageIO.read(new File("images/blueBubble.jpg"));
 
-        int maxHeight = screenShotImage.getHeight()/10;
-        int maxWidth = screenShotImage.getWidth()/10;
+        int[] rgbArrayPink = new int[10];
+        int[] rgbArrayPurple = new int[10];
+        int[] rgbArrayGreen = new int[10];
+        int[] rgbArrayBlue = new int[10];
+
+        pinkBubble.getRGB(0, 0,10,10, rgbArrayPink, 0, 0);
+        purpleBubble.getRGB(0, 0,10,10, rgbArrayPurple, 0, 0);
+        greenBubble.getRGB(0, 0,10,10, rgbArrayGreen, 0, 0);
+        blueBubble.getRGB(0, 0,10,10, rgbArrayBlue, 0, 0);
+
+        int maxHeight = screenShotImage.getHeight()-20;
+        int maxWidth = screenShotImage.getWidth()-20;
+        ArrayList<Integer> coordinatesOnScreen = new ArrayList<>();
         for(int i = 0; i < maxHeight; i++){
             for(int j = 0; j < maxWidth; j++){
-                int[] rgbArray = new int[100];
-                screenShotImage.getRGB(i*10, j*10,10,10, rgbArray, 0, 0);
+                int[] rgbArray = new int[10];
+                screenShotImage.getRGB(i, j,10,10, rgbArray, 0, 0);
+                if(rgbArray.equals(rgbArrayPink)|| rgbArray.equals(rgbArrayPurple) || rgbArray.equals(rgbArrayGreen) || rgbArray.equals(rgbArrayBlue)){
+                    coordinatesOnScreen.add(i);
+                    coordinatesOnScreen.add(j);
+                    return coordinatesOnScreen;
+                }
             }
         }
-
+        return coordinatesOnScreen;
     }
 /*
     private WarderobeChallangeLogic.Color[][] createGameBoard(){
--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,11 @@
+import java.awt.image.BufferedImage;
 import java.util.ArrayList;
-import java.util.Scanner;
-
+import java.util.concurrent.TimeUnit;
 
 public class Main {
 
     public static void main(String[] args) throws Exception {
+        /*
         WarderobeChallangeLogic board = new WarderobeChallangeLogic();
         SolutionAlgorithm solutionAlgorithm = new SolutionAlgorithm(board);
         ArrayList<int[]> solutionMoves = solutionAlgorithm.getSolutionMoves();
@@ -15,7 +16,15 @@ public class Main {
             System.out.print(",");
             System.out.print(solutionMoves.get(i)[1]);
             System.out.print("  ");
-        }
+            }
+
+         */
+
+        TimeUnit.SECONDS.sleep(5);
+        GsmBot gsmBot = new GsmBot();
+        BufferedImage screenShot = gsmBot.takeScreenShot();
+        gsmBot.findStartCoordinate(screenShot);
+
     }
 
 }
