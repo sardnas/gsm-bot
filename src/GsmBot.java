1af62dda64d5282209d63f0ed0f5534cb185e51c
Sandra Reinecke <sandra.reinecke@consid.se>
2023-06-14 16:31:32 +0200

    almost done

--- src/GsmBot.java
+++ src/GsmBot.java
@@ -3,11 +3,15 @@ import java.awt.image.BufferedImage;
 import java.io.IOException;
 import java.io.File;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.TimeUnit;
 import javax.imageio.ImageIO;
 
 
 public class GsmBot {
 
+    Map<int[],int[]> coordinateAndScreenPosition = new HashMap<>();
     public GsmBot(){
 
     }
@@ -16,62 +20,127 @@ public class GsmBot {
         Robot robot = new Robot();
         Rectangle capture = new Rectangle(Toolkit.getDefaultToolkit().getScreenSize());
         BufferedImage image = robot.createScreenCapture(capture);
-        System.out.println("Took shot. Height: ");
-        System.out.println(image.getHeight());
         return image;
     }
 
-    private void findGameBoard(BufferedImage screenShotImage) {
-
-
-    }
-
-    public void findStartCoordinate(BufferedImage screenShotImage) throws IOException {
-        ArrayList<Integer> leftTopCorner = findMatch(screenShotImage);
+    private void findGameBoard() throws AWTException, IOException {
+        BufferedImage screenShotImage = takeScreenShot();
+        System.out.println("Took shot.");
+        ArrayList<Integer> leftTopCorner = findStartCoordinate(screenShotImage);
+        System.out.println("Found top left corner. ");
         System.out.print(leftTopCorner.get(0));
         System.out.print(",");
         System.out.print(leftTopCorner.get(1));
+        WarderobeChallangeLogic.Color[][] initialBoard = createGameBoard(leftTopCorner, screenShotImage);
     }
 
-    private ArrayList<Integer> findMatch(BufferedImage screenShotImage) throws IOException {
-        BufferedImage pinkBubble = null;
-        pinkBubble = ImageIO.read(new File("images/pinkBubble.jpg"));
+    private WarderobeChallangeLogic.Color[][] createGameBoard(ArrayList<Integer> leftTopCorner, BufferedImage screenShotImage){
+        WarderobeChallangeLogic.Color[][] board = new WarderobeChallangeLogic.Color[11][11];
+        int[] rgbArray = new int[1];
 
-        BufferedImage purpleBubble = null;
-        purpleBubble = ImageIO.read(new File("images/purpleBubble.jpg"));
+        int x = leftTopCorner.get(0) + 30;
+        int y = leftTopCorner.get(1);
+        int i = x;
+        int j = y;
+        int counter = 0;
 
-        BufferedImage greenBubble = null;
-        greenBubble = ImageIO.read(new File("images/greenBubble.jpg"));
+        boolean running = true;
+        while(running){
+            counter++;
+            screenShotImage.getRGB(i, j, 1, 1, rgbArray, 0, 1);
+            i = i + 34;
+            if (counter > 11*12-1) {
+                running = false;
+            } else if(counter % 11 == 0){
 
-        BufferedImage blueBubble = null;
-        blueBubble = ImageIO.read(new File("images/blueBubble.jpg"));
+                j = j + 34;
+                i = x;
+            }
 
-        int[] rgbArrayPink = new int[10];
-        int[] rgbArrayPurple = new int[10];
-        int[] rgbArrayGreen = new int[10];
-        int[] rgbArrayBlue = new int[10];
+        }
+        return board;
+    }
 
-        pinkBubble.getRGB(0, 0,10,10, rgbArrayPink, 0, 0);
-        purpleBubble.getRGB(0, 0,10,10, rgbArrayPurple, 0, 0);
-        greenBubble.getRGB(0, 0,10,10, rgbArrayGreen, 0, 0);
-        blueBubble.getRGB(0, 0,10,10, rgbArrayBlue, 0, 0);
 
-        int maxHeight = screenShotImage.getHeight()-20;
-        int maxWidth = screenShotImage.getWidth()-20;
+    public ArrayList<Integer> findStartCoordinate(BufferedImage screenShotImage) throws IOException {
+        BufferedImage cornerImage = ImageIO.read(new File("images/corner.jpg"));
+        BufferedImage purpleBubble = ImageIO.read(new File("images/purpleBubble.jpg"));
+        BufferedImage greenBubble = ImageIO.read(new File("images/greenBubble.jpg"));
+        BufferedImage blueBubble = ImageIO.read(new File("images/blueBubble.jpg"));
+
+        int regionSize = 8;
+        int maxHeight = screenShotImage.getHeight() - regionSize;
+        int maxWidth = screenShotImage.getWidth() - regionSize;
         ArrayList<Integer> coordinatesOnScreen = new ArrayList<>();
-        for(int i = 0; i < maxHeight; i++){
-            for(int j = 0; j < maxWidth; j++){
-                int[] rgbArray = new int[10];
-                screenShotImage.getRGB(i, j,10,10, rgbArray, 0, 0);
-                if(rgbArray.equals(rgbArrayPink)|| rgbArray.equals(rgbArrayPurple) || rgbArray.equals(rgbArrayGreen) || rgbArray.equals(rgbArrayBlue)){
-                    coordinatesOnScreen.add(i);
+
+        for (int i = 0; i < maxHeight; i++) {
+            for (int j = 0; j < maxWidth; j++) {
+                int[] rgbArray = new int[regionSize*regionSize]; // Increased array size to capture a 10x10 region
+                screenShotImage.getRGB(j, i, regionSize, regionSize, rgbArray, 0, regionSize);
+
+                if (arrayEqualToBufferedImage(rgbArray, cornerImage)) {
                     coordinatesOnScreen.add(j);
-                    return coordinatesOnScreen;
+                    coordinatesOnScreen.add(i);
+                    return coordinatesOnScreen; // Found a match, returning coordinates
                 }
             }
         }
-        return coordinatesOnScreen;
+        return coordinatesOnScreen; // No match found
     }
+
+    private boolean pixelEqual(int pixelOne, int pixelTwo){
+        int threshold = 100; // Adjust this threshold as needed
+        int rgb1 = pixelOne;
+        int rgb2 = pixelTwo;
+
+        int r1 = (rgb1 >> 16) & 0xFF;
+        int g1 = (rgb1 >> 8) & 0xFF;
+        int b1 = rgb1 & 0xFF;
+
+        int r2 = (rgb2 >> 16) & 0xFF;
+        int g2 = (rgb2 >> 8) & 0xFF;
+        int b2 = rgb2 & 0xFF;
+
+        int diffR = Math.abs(r1 - r2);
+        int diffG = Math.abs(g1 - g2);
+        int diffB = Math.abs(b1 - b2);
+
+        if (diffR > threshold || diffG > threshold || diffB > threshold) {
+            return false; // RGB values are not within the threshold
+        }
+        return true;
+    }
+
+    private boolean arrayEqualToBufferedImage(int[] array1, BufferedImage image2) {
+        int[] array2 = new int[8*8]; // Increased array size to match the region size
+        image2.getRGB(0, 0, 8, 8, array2, 0, 8);
+
+        int threshold = 100; // Adjust this threshold as needed
+
+        for (int i = 0; i < array1.length; i++) {
+            int rgb1 = array1[i];
+            int rgb2 = array2[i];
+
+            int r1 = (rgb1 >> 16) & 0xFF;
+            int g1 = (rgb1 >> 8) & 0xFF;
+            int b1 = rgb1 & 0xFF;
+
+            int r2 = (rgb2 >> 16) & 0xFF;
+            int g2 = (rgb2 >> 8) & 0xFF;
+            int b2 = rgb2 & 0xFF;
+
+            int diffR = Math.abs(r1 - r2);
+            int diffG = Math.abs(g1 - g2);
+            int diffB = Math.abs(b1 - b2);
+
+            if (diffR > threshold || diffG > threshold || diffB > threshold) {
+                return false; // RGB values are not within the threshold
+            }
+        }
+
+        return true; // RGB values are within the threshold
+    }
+
 /*
     private WarderobeChallangeLogic.Color[][] createGameBoard(){
 
--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.util.ArrayList;
 import java.util.concurrent.TimeUnit;
@@ -5,7 +6,7 @@ import java.util.concurrent.TimeUnit;
 public class Main {
 
     public static void main(String[] args) throws Exception {
-        /*
+
         WarderobeChallangeLogic board = new WarderobeChallangeLogic();
         SolutionAlgorithm solutionAlgorithm = new SolutionAlgorithm(board);
         ArrayList<int[]> solutionMoves = solutionAlgorithm.getSolutionMoves();
@@ -18,13 +19,48 @@ public class Main {
             System.out.print("  ");
             }
 
-         */
 
-        TimeUnit.SECONDS.sleep(5);
+/*
+
+        TimeUnit.SECONDS.sleep(3);
         GsmBot gsmBot = new GsmBot();
         BufferedImage screenShot = gsmBot.takeScreenShot();
-        gsmBot.findStartCoordinate(screenShot);
+        ArrayList<Integer> match = gsmBot.findStartCoordinate(screenShot);
+*/
+        int x = 810 + 30;
+        int y = 329;
+        int i = x;
+        int j = y;
+        int counter = 0;
 
+        TimeUnit.SECONDS.sleep(3);
+        Robot robot = new Robot();
+        robot.mouseMove(x + 35, y + 35);
+
+        boolean running = true;
+        while(running){
+            TimeUnit.SECONDS.sleep(1);
+            counter++;
+            robot.mouseMove(i, j);
+            i = i + 34;
+            if (counter > 11*12-1) {
+                running = false;
+            } else if(counter % 11 == 0){
+
+                j = j + 34;
+                i = x;
+            }
+
+        }
     }
 
 }
+
+// 810,329
+// x + 30, y första bubblan
+// x + 34, y + 34 röra sig höger eller vänster
+
+// höger hörn topp x + 380, y
+// höger hörn botten x + 380, y + 410
+// vänster hörn topp x y
+// vänster hörn botten = x, y + 410
